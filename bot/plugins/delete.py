16e2771fc0f1c93c405f2f511df5da51702931aa
levina-x <82687844+levina-x@users.noreply.github.com>
2021-04-18 21:19:34 +0000

    Update delete.py

--- bot/plugins/delete.py
+++ bot/plugins/delete.py
@@ -8,7 +8,7 @@ from bot import LOGGER
 def _delete(client, message):
   user_id = message.from_user.id
   if len(message.command) > 1 or message.reply_to_message:
-    sent_message = message.reply_text('🕵️**Checking Link...**', quote=True)
+    sent_message = message.reply_text('🕵️**sedang memeriksa link...**', quote=True)
     if len(message.command) > 1:
       link = message.command[1]
     elif message.reply_to_message.entities[1].url:
